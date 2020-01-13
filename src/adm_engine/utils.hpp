e3b8195ac676673d25a001966141178899dde454
Valentin NOEL <valentin.noel@media-io.com>
2020-01-13 15:03:15 +0100

    Utils file refactoring

--- src/adm_engine/utils.hpp
+++ src/adm_engine/utils.hpp
@@ -1,36 +1,9 @@
 #pragma once
 
-#include <regex>
+#include <string>
 
 namespace admengine {
 
-const std::map<std::string, std::string> specCharMap = {
-  { " ", "_" },
-  { "é", "e" },
-  { "è", "e" },
-  { "ê", "e" },
-  { "ë", "e" },
-  { "à", "a" },
-  { "â", "a" },
-  { "ä", "a" },
-  { "ù", "u" },
-  { "û", "u" },
-  { "ü", "u" }
-  // TODO: complete this list...
-};
-
-const std::regex specCharRegex("[^a-zA-Z0-9_-]");
-
-std::string replaceSpecialCharacters(std::string& text) {
-  // map some special characters
-  std::map<std::string, std::string>::const_iterator it;
-  for (it = specCharMap.begin(); it != specCharMap.end(); it++) {
-    text = std::regex_replace(text, std::regex(it->first), it->second);
-  }
-
-  // remove remaining special characters
-  text = std::regex_replace(text, specCharRegex, "_");
-  return text;
-}
+std::string replaceSpecialCharacters(std::string& text);
 
 }
